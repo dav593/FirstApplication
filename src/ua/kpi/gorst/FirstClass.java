99cb1f5bf44c4fe77aacc7dabb08ba865a3de427
Maksym Nevdashchenko <maksym.nevdashchenko@gmail.com>
2016-09-14 17:34:00 +0300

    Initialized projecr FirstApplication. Added FirstClass

---
+++ src/ua/kpi/gorst/FirstClass.java
@@ -0,0 +1,10 @@
+package ua.kpi.gorst;
+
+/**
+ * Created by Dav on 14.09.2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("First Java Project.");
+    }
+}
