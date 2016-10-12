226e2a24df4683236c1a9cec5c8cf2324dab88f2
David Gorst <gorstdavid@gmail.com>
2016-10-12 17:29:01 +0300

    Add 2

---
+++ src/ua/kpi/gorst/Next.java
@@ -0,0 +1,14 @@
+package ua.kpi.gorst;
+
+/**
+ * Created by Dav on 12.10.2016.
+ */
+public class Next {
+    public static void main (String[] arg){
+        int a = 25;
+        double b =35.8;
+        double i = a + b;
+        if (i != 0);
+        System.out.println(i);
+    }
+}
---
+++ src/ua/kpi/gorst/Test.java
@@ -0,0 +1,19 @@
+package ua.kpi.gorst;
+
+/**
+ * Created by Dav on 12.10.2016.
+ */
+public class Test {
+    public static void main (String[] args ) {
+
+        boolean a;
+        a = 5>=2;
+        a = 5 !=5;
+         boolean b;
+        b = a == false;
+
+        System.out.println(b);
+
+
+    }
+}
