0d1c7a03cfe3dd50f6d031b8849ae509eff9489c
David Gorst <gorstdavid@gmail.com>
2016-10-18 01:49:41 +0300

    Add 2

---
+++ src/ua/kpi/gorst/Operetion.java
@@ -0,0 +1,14 @@
+package ua.kpi.gorst;
+
+/**
+ * Created by Dav on 18.10.2016.
+ */
+public class Operetion {
+    public static void main (String[] arg){
+        if (true){
+
+
+        System.out.println("Тест прошел удачно");
+    }
+    }
+}
