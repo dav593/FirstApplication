00f09df7fed33fb73b19924250d673725b8fb1b4
David Gorst <gorstdavid@gmail.com>
2016-11-22 20:43:30 +0200

    Add 2

---
+++ src/ua/kpi/gorst/Working.java
@@ -0,0 +1,69 @@
+package ua.kpi.gorst;
+
+/**
+ * Created by Dav on 22.11.2016.
+ */
+public class Working {
+    private String fullname;
+    private String position;
+    private String subdivision;
+    private int age;
+    private int wage;
+    public Working(String fullname,String position,String subdivision,int age,int wage){
+        fullname = "Петров Васлилй Эдуартдович";
+        position = "Директор";
+        subdivision = "Машиностроение";
+        age = 45;
+        wage = 25000;
+
+
+    }
+    public String getFullname(){
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
+    }
+
+    public void setSubdivision(String subdivision) {
+        this.subdivision = subdivision;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWage() {
+        return wage;
+    }
+
+    public void setWage(int wage) {
+        this.wage = wage;
+    }
+    public static void main(String[] args){
+        int w = 25000;
+        double t = 5;
+        double result = i;
+        for (w/t = i){
+            result = i;
+        }
+        System.out.println(result);
+    }
+}
